60e5751c685cf42b0c6ddf7bfc06a4be2ae53053
mamontalvan <mamontalvan@gmail.com>
2024-02-24 21:14:28 -0500

    Antes de insertar por lote

---
+++ src/seed/interfaces/api-poke-response.interface.ts
@@ -0,0 +1,11 @@
+export interface APIPokeResponse {
+    count:    number;
+    next:     string;
+    previous: null;
+    results:  Result[];
+}
+
+export interface Result {
+    name: string;
+    url:  string;
+}
---
+++ src/seed/seed.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { APIPokeResponse } from './interfaces/api-poke-response.interface';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<APIPokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+    data.results.forEach(({name, url}) => {
+      const segments = url.split('/');
+      const no = segments[segments.length - 2];
+      console.log(name, no);
+    });
+    return  data.results;
+  }
+}
